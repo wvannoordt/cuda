33daed3b9e51fffe61b15618acc383e3431f3c18
Will van Noordt <willvanzero@gmail.com>
2019-11-26 22:35:51 +0000

    More IO

--- projects/wave-eqn/main.cc
+++ projects/wave-eqn/main.cc
@@ -16,10 +16,20 @@ typedef double specified_precision;
 
 int main()
 {
-    int domain_width = 1 << 9;
-    int grid_dimension = domain_width / BLOCK_SIZE;/*
-    std::cout << "Domain:                 " << domain_width << "x" << domain_width << std::endl;
-    std::cout << "Block size:             " << BLOCK_SIZE << "x" << BLOCK_SIZE << std::endl;
-    std::cout << "Solution vector size:   " << domain_width*domain_width << std::endl;*/
+    int domain_width_points = 1 << 9;
+    int grid_dimension = domain_width_points / BLOCK_SIZE;
+    std::cout << "Domain:                         " << domain_width_points << "x" << domain_width_points << std::endl;
+    std::cout << "Block size:                     " << BLOCK_SIZE << "x" << BLOCK_SIZE << std::endl;
+    std::cout << "Solution vector size:           " << domain_width_points*domain_width_points << std::endl;
+    std::cout << "Solution vector entries/block:  " << BLOCK_SIZE*BLOCK_SIZE << std::endl;
+    std::cout << "Total block count:              " << grid_dimension*grid_dimension << std::endl;
+    std::cout << "Grid size:                      " << grid_dimension << "x" << grid_dimension << std::endl << std::endl;
+
+    specified_precision domain_length_x = 1.0;
+    specified_precision domain_length_y = 1.0;
+    specified_precision inverse_dx = domain_width_points/domain_length_x;
+    specified_precision inverse_dy = domain_width_points/domain_length_y;
+
+
     return 0;
 }
