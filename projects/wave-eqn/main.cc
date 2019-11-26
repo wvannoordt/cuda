6499784e727de7ea51d78d441cac7071692ebadb
Will van Noordt <willvanzero@gmail.com>
2019-11-26 23:33:45 +0000

    fooling around and Im tired

--- projects/wave-eqn/main.cc
+++ projects/wave-eqn/main.cc
@@ -12,7 +12,7 @@ typedef float specified_precision;
 typedef double specified_precision;
 #endif
 
-#define BLOCK_SIZE 16
+#define BLOCK_SIZE 32
 
 
 void single_to_double_idx(int i, int* x, int* y, int domain_dim)
@@ -24,6 +24,7 @@ void double_to_single_idx(int x, int y, int* idx, int domain_dim)
 {
     *(idx) = y*domain_dim + x;
 }
+
 
 int main()
 {
@@ -42,13 +43,19 @@ int main()
     specified_precision inverse_dx = domain_width_points/domain_length_x;
     specified_precision inverse_dy = domain_width_points/domain_length_y;
 
-    std::cout << "Entry (0,0) is a function of entry (1,0) and (0,1)," << std::endl;
+    std::cout << "E.g. entry (0,0) is a function of entry (1,0) and (0,1)," << std::endl;
     int a, b, c;
     double_to_single_idx(0,0,&a,domain_width_points);
     double_to_single_idx(1,0,&b,domain_width_points);
     double_to_single_idx(0,1,&c,domain_width_points);
     std::cout << "so vector entry "<< a << " is a function of vector entry ";
     std::cout << b << " and vector entry " << c << "." << std::endl;
+    
+	int last_block_entry = BLOCK_SIZE*BLOCK_SIZE - 1;
+	int x, y;
+	std::cout << last_block_entry << std::endl;
+	single_to_double_idx(last_block_entry, &x, &y, domain_width_points);
+	std::cout << x << ", " << y << std::endl;
 
     return 0;
 }
