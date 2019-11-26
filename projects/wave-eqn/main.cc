f35f2cc184d01ad90462304c9dd5c647433ee9f9
Will van Noordt <willvanzero@gmail.com>
2019-11-26 22:23:36 +0000

    asdads

--- projects/wave-eqn/main.cc
+++ projects/wave-eqn/main.cc
@@ -17,9 +17,9 @@ typedef double specified_precision;
 int main()
 {
     int domain_width = 1 << 9;
-    int grid_dimension = domain_width / BLOCK_SIZE;
+    int grid_dimension = domain_width / BLOCK_SIZE;/*
     std::cout << "Domain:                 " << domain_width << "x" << domain_width << std::endl;
     std::cout << "Block size:             " << BLOCK_SIZE << "x" << BLOCK_SIZE << std::endl;
-    std::cout << "Solution vector size:   " << domain_width*domain_width << std::endl;
+    std::cout << "Solution vector size:   " << domain_width*domain_width << std::endl;*/
     return 0;
 }
