c6c9d702bab21062f1ba3b7db6c8bd7a4708ae57
Will van Noordt <willvanzero@gmail.com>
2019-12-03 22:37:46 +0000

    help me

--- projects/wave-eqn/main.cc
+++ projects/wave-eqn/main.cc
@@ -43,11 +43,52 @@ int main()
     specified_precision inverse_dx = domain_width_points/domain_length_x;
     specified_precision inverse_dy = domain_width_points/domain_length_y;
 
+    int x1,y1,x2,y2,init,term,x_inq,y_inq,l_bound,u_bound,depend;
     for (int i = 0; i < grid_dimension*grid_dimension; i++)
     {
-        if (i < 3 || i > grid_dimension*grid_dimension - 3)
+        l_bound = grid_dimension + 1;
+        u_bound = -1;
+        init = i*BLOCK_SIZE*BLOCK_SIZE;
+        term = i*BLOCK_SIZE*BLOCK_SIZE + BLOCK_SIZE*BLOCK_SIZE - 1;
+        single_to_double_idx(init, &x1, &y1, domain_width_points);
+        single_to_double_idx(term, &x2, &y2, domain_width_points);
+        if (i < 3 || i > grid_dimension*grid_dimension - 4)
         {
-            std::cout << "Block " << i << " handles x_" << i*BLOCK_SIZE*BLOCK_SIZE << " thru x_" << i*BLOCK_SIZE*BLOCK_SIZE + BLOCK_SIZE*BLOCK_SIZE - 1 << std::endl;
+            std::cout << "Block " << i << " handles y_" << init;
+            std::cout << " thru y_" << term;
+            std::cout << ", grid_idx_1=(" << x1 << "," << y1 << ")";
+            std::cout << ", grid_idx_2=(" << x2 << "," << y2 << ")" << std::endl;
+
+            for (int k = init; k <= term; k++)
+            {
+                single_to_double_idx(k,&x_inq,&y_inq,domain_width_points);
+                if (x_inq+1 < domain_width_points)
+                {
+                    double_to_single_idx(x_inq+1,y_inq,&depend,domain_width_points);
+                    l_bound = depend < l_bound ? depend: l_bound;
+                    u_bound = depend > u_bound ? depend: u_bound;
+                }
+                if (y_inq+1 < domain_width_points)
+                {
+                    double_to_single_idx(x_inq,y_inq+1,&depend,domain_width_points);
+                    l_bound = depend < l_bound ? depend: l_bound;
+                    u_bound = depend > u_bound ? depend: u_bound;
+                }
+                if (x_inq-1 >= 0)
+                {
+                    double_to_single_idx(x_inq-1,y_inq,&depend,domain_width_points);
+                    l_bound = depend < l_bound ? depend: l_bound;
+                    u_bound = depend > u_bound ? depend: u_bound;
+                }
+                if (y_inq-1 >= 0)
+                {
+                    double_to_single_idx(x_inq,y_inq-1,&depend,domain_width_points);
+                    l_bound = depend < l_bound ? depend: l_bound;
+                    u_bound = depend > u_bound ? depend: u_bound;
+                }
+            }
+
+            std::cout << "Requires x_" << l_bound << " thru x_" << u_bound << std::endl;
         }
         else if (i == 4)
         {
